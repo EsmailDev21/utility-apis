038c88ef8f30a6ee74569284447bb413ed8e7542
esmail <smail.anonymous8@gmail.com>
2024-07-29 02:00:23 +0200

    fgdfg

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
-// upload.js
-import { storage } from './firebaseConfig';
+// upload.jsimport { initializeApp } from 'firebase/app';
+import { getStorage } from 'firebase/storage';
+
+
 import { ref, uploadBytesResumable, getDownloadURL } from 'firebase/storage';
 import express from 'express';
 import multer from 'multer';
+
+const firebaseConfig = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID
+};
+
+const fbapp = initializeApp(firebaseConfig);
+const storage = getStorage(fbapp);
 
 const upload = multer({ storage: multer.memoryStorage() });
 const app = express();
